5607b45f50f7e07fdf6d4226f2918ff5bed9b20f
Ridwanur Rahman <ridwanur196@gmail.com>
2025-01-07 12:42:07 +0600

    sending get request feature added

--- cccurl.js
+++ cccurl.js
@@ -1,14 +1,48 @@
+const net = require('net');
 args = process.argv.slice(2);
-const extractedUrl = args[0];
-function extractURL(extractedUrl){
-    const url = new URL(extractedUrl);
-    const protocol = url.protocol.replace(':', '');
-    const host = url.hostname;
-    const port = url.port || 80
-    const path = url.pathname || '/';
-    console.log(`connecting to ${host}`);
-    console.log(`Sending request to GET ${path} HTTP/1.1`);
-    console.log(`Host: ${host}`);
-    console.log("Accept: */*")
+
+function sendGetRequest(url) {
+    const { protocol, hostname, port, pathname } = new URL(url);
+    const portNumber = port || (protocol === 'http:' ? 80 : 443);
+    const client = new net.Socket();
+    client.connect(portNumber, hostname, () => {
+        console.log(`Sending request GET ${pathname} HTTP/1.1`);
+        console.log(`Host: ${hostname}`);
+        console.log("Accept: */*");
+        console.log("Connection: close");
+        client.write(`GET ${pathname} HTTP/1.1\r\n`);
+        client.write(`Host: ${hostname}\r\n`);
+        client.write('Connection: close\r\n');
+        client.write('\r\n');
+    });
+
+    client.on('data', (data) => {
+        console.log(data.toString());
+        client.end();
+    });
+
+    client.on('close', () => {
+       
+    });
+
+    client.on('error', (err) => {
+        console.error(`Connection error: ${err.message}`);
+    });
+
 }
-console.log(extractURL(extractedUrl));
+const extractedUrl = args[0];
+sendGetRequest(extractedUrl);
+
+// function extractURL(url){
+//     const protocol = url.protocol.replace(':', '');
+//     const host = url.hostname;
+//     const port = url.port || 8080
+//     const path = url.pathname || '/';
+//     console.log(`connecting to ${host}`);
+//     console.log(`Sending request  GET ${path} HTTP/1.1`);
+//     console.log(`Host: ${host}`);
+//     console.log("Accept: */*")
+// }
+// // extractURL(url);
+
+
