182bdf352340e995dc9abff1f8f09f63cdff9e42
Ridwanur Rahman <ridwanur196@gmail.com>
2025-01-08 14:45:56 +0600

    added other http method delete

--- cccurl.js
+++ cccurl.js
@@ -1,24 +1,24 @@
 const net = require('net');
 const urlIndex = process.argv.length - 1;
 const args = process.argv;
-function sendGetRequest(url) {
+
+function sendRequest(url) {
     const { protocol, hostname, port, pathname } = new URL(url);
     const portNumber = port || (protocol === 'http:' ? 80 : 443);
     const client = new net.Socket();
     let response = '';
+    
     client.connect(portNumber, hostname, () => {
-        if(args[2] === '-v'){
-            //console.log(`Sending request GET ${pathname} HTTP/1.1`);
-            console.log(`> GET ${pathname} HTTP/1.1`)
-            console.log(`> Host: ${hostname}`);
-            console.log("> Accept: */*");
-            console.log(">");
-            //console.log("Connection: close");
-        }
-        client.write(`GET ${pathname} HTTP/1.1\r\n`);
-        client.write(`Host: ${hostname}\r\n`);
-        client.write('Connection: close\r\n');
-        client.write('\r\n');
+        const method = args[2] === '-X' ? args[3] : 'GET';
+        const request = [
+            `${method} ${pathname} HTTP/1.1`,
+            `Host: ${hostname}`,
+            'Accept: */*',
+            'Connection: close',
+            '',
+            '' 
+        ].join('\r\n');
+        client.write(request);
     });
 
     client.on('data', (data) => {
@@ -26,25 +26,32 @@ function sendGetRequest(url) {
     });
 
     client.on('end', () => {
-        const [headerPart,bodyPart] = response.split('\r\n\r\n');
+        const [headerPart, bodyPart] = response.split('\r\n\r\n');
         const headers = headerPart.split('\r\n');
-        modifiedHeaders = headers.map(header => `< ${header}`);
-        if(args[2] == "-v"){
-            modifiedHeaders.forEach(header => {
-            console.log(header);
-        });
-            console.log('<');   
+        if(args[2] === '-v') {
+            console.log(`> GET ${pathname} HTTP/1.1`);
+            console.log(`> Host: ${hostname}`);
+            console.log("> Accept: */*");
+            console.log(">");
+            headers.forEach(header => {
+                console.log(`< ${header}`);
+            });
+            console.log('<');
         }
-        console.log(bodyPart);   
+        console.log(bodyPart);
     });
 
     client.on('error', (err) => {
         console.error(`Connection error: ${err.message}`);
     });
-
 }
+
 const extractedUrl = args[urlIndex];
-sendGetRequest(extractedUrl);
+try {
+    sendRequest(extractedUrl);
+} catch (error) {
+    console.error('Error:', error.message);
+}
 
 // function extractURL(url){
 //     const protocol = url.protocol.replace(':', '');
